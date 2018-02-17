ae49e2fb2caa70c6396c729582dbc55259f81f46
Piotr Dyraga <piotr@ontheserverside.com>
2018-02-17 15:01:01 +0100

    Gravitational constant [pc] value fix - it was 10 times higher than it should

--- src/main/scala/com/ontheserverside/galaxy/Constants.scala
+++ src/main/scala/com/ontheserverside/galaxy/Constants.scala
@@ -4,7 +4,7 @@ object Constants {
   /**
     *  Gravitational constant expressed in parsecs [pc] and Solar mass [M_sun]
     */
-  val G = 4.3 * 10e-3 // pc/M_sun (km/s)^2
+  val G = 4.3e-3 // pc/M_sun (km/s)^2
 
   /**
     * Transforms units: [km] to [pc]
